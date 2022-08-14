daa877da53114a4f5ab6a764d7fabec30d4f748d
Mariam Maged <maryam.maged76@gmail.com>
2022-08-14 11:04:08 +0200

    s

--- final.py
+++ final.py
@@ -28,10 +28,10 @@ with st.sidebar:
     with st.container():
         paragraph="""\nThis app will generate impression part of an X-ray report.
                 \nYou can upload 2 X-rays that are front view and side view of chest of the same individual.
-                \nNote: The 2nd X-ray is optional."""
+            Note: The 2nd X-ray is optional."""
         
         text_area = st.empty()
-        text=text_area.text_area("", paragraph, height=220)
+        text=text_area.text_area("", paragraph, height=230)
     
 
 
